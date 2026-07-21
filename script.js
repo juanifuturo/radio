593e277610c5e87ce9b552cda28237ae939b5297
juanifuturo <jmingarro@gmail.com>
2026-07-21 14:37:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Radio</title>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<h1>RADIO</h1>
+
+<div id="podcasts"></div>
+
+<script src="script.js"></script>
+
+</body>
+
+</html>
