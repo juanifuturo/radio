4671ed4181c7ae977e8b281f6c1aa2e38731a521
juanifuturo <jmingarro@gmail.com>
2026-07-24 23:27:43 +0200

    Fix HTML entity encoding in script.js

--- script.js
+++ script.js
@@ -58,8 +58,8 @@ bloque.innerHTML = `
         <p>${titulo}</p>
 
         <div class="meta">
-            <span>⏱ ${duracion}</span>
-            <span>📅 ${fecha}</span>
+            <span>${duracion}</span>
+            <span>${fecha}</span>
         </div>
     </div>
 `;
